cd37f5adda381ed1621ba1fc41e21bacb666ae78
keith <keith@raincityambience.com>
2018-06-21 00:10:46 -0700

    setting post state

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -2,9 +2,61 @@ import React from 'react';
 
 class NewPost extends React.Component {
 
+  state = {
+    title: '',
+    body: ''
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if(this.state.title.trim() && this.state.body.trim()){
+      console.log('submtting ');
+      this.handleReset();
+    }
+  }
+
+  handleInputChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleReset = () => {
+    this.setState({title: '', body: ''})
+  }
 
   render(){
-    return <h1>NewPost</h1>
+    return (
+      <div>
+        <form onSubmit={ this.handleSubmit } style={{maxWidth: "600px"}}>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Title"
+              className="form-control"
+              name="title"
+              onChange={ this.handleInputChange }
+              value={ this.state.title }
+            />
+          </div>
+          <div className="form-group">
+            <textarea
+              cols="19"
+              rows="8"
+              placeholder="Body"
+              className="form-control"
+              name="body"
+              onChange={ this.handleInputChange }
+              value={ this.state.body }>
+            </textarea>
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">Add Post</button>
+            <button type="button" className="btn btn-warning" onClick={ this.handleReset }>
+              Reset
+            </button>
+          </div>
+        </form>
+      </div>
+    )
   }
 
 }
